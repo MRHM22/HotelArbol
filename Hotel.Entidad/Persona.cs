e83403261aaf343b68b7ee1cc71f93b7c2e9c3d1
MRHM22 <hdezmartin22@gmail.com>
2015-10-29 22:40:52 -0600

    veamos si funciona

--- Hotel.Entidad/Persona.cs
+++ Hotel.Entidad/Persona.cs
@@ -7,8 +7,40 @@ namespace Hotel.Entidad
 {
     public class Persona
     {
+        String Cedula;
+
+        public String cedula
+        {
+            get { return Cedula; }
+            set { Cedula = value; }
+        }
         String Nombre;
-        String Apellido;
-        String SegundoApellido;
+
+        public String nombre
+        {
+            get { return Nombre; }
+            set { Nombre = value; }
+        }
+        String PriApellido;
+
+        public String priApellido
+        {
+            get { return PriApellido; }
+            set { PriApellido = value; }
+        }
+        String SegApellido;
+
+        public String segApellido
+        {
+            get { return SegApellido; }
+            set { SegApellido = value; }
+        }
+        String Clave;
+
+        public String clave
+        {
+            get { return Clave; }
+            set { Clave = value; }
+        }
     }
 }
